4490f4cdb4e0fd0156242b287aecc9eddf19683b
Paul Garcia <gp22@users.noreply.github.com>
2023-04-10 16:20:25 -0600

    Update contract address

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,9 +1,11 @@
 import web3 from './web3';
 import campaignFactory from './build/CampaignFactory.json';
 
+const contractAddress = '0x40C4B36e577cf7970E389c7d9F1cD31297066d4f';
+
 const instance = new web3.eth.Contract(
   JSON.parse(campaignFactory.interface),
-  '0x6Afc1529fc9F5dbE4f1cb8A0757c11C05d91F293'
+  contractAddress
 );
 
 export default instance;
