3c026f8bb3e6216c6ae259f7ddebf8cc540b5b91
Paul Garcia <gp22@users.noreply.github.com>
2023-04-10 16:22:17 -0600

    Add routing

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -3,6 +3,7 @@ import { Button, Form, Input, Message } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import factory from '../../ethereum/factory';
 import web3 from '../../ethereum/web3';
+import { Router } from '../../routes';
 
 const CampaignNew = () => {
   const [minimumContribution, setMinimumContribution] = useState('');
@@ -19,6 +20,8 @@ const CampaignNew = () => {
       await factory.methods
         .createCampaign(minimumContribution)
         .send({ from: accounts[0] });
+
+      Router.pushRoute('/');
     } catch (e) {
       setErrorMessage(e.message);
     }
